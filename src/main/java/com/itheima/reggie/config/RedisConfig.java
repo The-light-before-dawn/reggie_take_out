b7c176d0f768b85c3feec9ffe2714ef9cf857e88
buhang <yifan253421937@outlook.com>
2024-05-23 14:18:28 +0800

    使用 Redis 缓存 短信验证码 和 菜品数据

--- src/main/java/com/itheima/reggie/config/RedisConfig.java
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.config;
 
 import io.lettuce.core.dynamic.RedisCommandFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,31 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+//@Configuration
+//public class RedisConfig extends CachingConfigurerSupport {
+//
+//
+//    @Autowired
+//    private RedisConnectionFactory redisConnectionFactory;
+//
+//    @Bean
+//    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//
+//        // 序列化key
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//
+//        // 序列化hash
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+//
+//        // 连接redis数据库
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//
+//        return redisTemplate;
+//    }
+//}
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
 
@@ -19,4 +45,5 @@ public class RedisConfig extends CachingConfigurerSupport {
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
+
 }
