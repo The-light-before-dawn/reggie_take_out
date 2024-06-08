4e66758ef1ee5b27c1fa37e23814dd0b27c499af
buhang <yifan253421937@outlook.com>
2024-06-09 06:02:51 +0800

    项目优化

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,17 +1,28 @@
 package com.itheima.reggie.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.itheima.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     /**
@@ -21,6 +32,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始进行静态资源映射");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
@@ -39,5 +52,32 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
         // 将上面的消息转换器对象追加到mvc框架内的转换器集合中
         converters.add(0, mappingJackson2HttpMessageConverter);
+    }
+
+    //配置Swagger2的Docket的Bean实例
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                // apiInfo()：配置 API 的一些基本信息，比如：文档标题title，文档描述description，文档版本号version
+                .apiInfo(apiInfo())
+                // select()：生成 API 文档的选择器，用于指定要生成哪些 API 文档
+                .select()
+                // apis()：指定要生成哪个包下的 API 文档
+                .apis(RequestHandlerSelectors.basePackage("cn.itheima.reggie.controller"))
+                // paths()：指定要生成哪个 URL 匹配模式下的 API 文档。这里使用 PathSelectors.any()，表示生成所有的 API 文档。
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private static final String API_TILE="瑞吉外卖";
+    //文档信息配置
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 文档标题
+                .title(API_TILE)
+                // 文档描述信息
+                .description("瑞吉外卖接口文档")
+                // 文档版本号
+                .version("1.0")
+                .build();
     }
 }
